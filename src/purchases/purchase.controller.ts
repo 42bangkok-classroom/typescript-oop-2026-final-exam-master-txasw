7c31814b120411af9061ede314eaeadd1cd5887d
txasw <txasw@outlook.co.th>
2026-03-25 11:04:52 +0700

    02-010

--- src/purchases/purchase.controller.ts
+++ src/purchases/purchase.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PurchaseService } from './purchase.service';
 
 @Controller('purchases')
 export class PurchaseController {
   constructor(private readonly purchaseService: PurchaseService) {}
 
-  @Get()
-  getPurchases() {
+  @Get(':productId')
+  getPurchases(@Param() productId: string) {
+    console.log(productId);
     return {
       success: true,
       data: this.purchaseService.findAll(),
